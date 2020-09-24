2f98896302758ce46d93237ace23089fc8cc6e14
Francesco Catani <francescocatanipersonal@gmail.com>
2020-09-24 16:15:34 +0200

    Improvements, test with example

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -21,3 +21,11 @@
 ## [1.0.5] - First release.
 
 * First release of the package.
+
+## [1.0.6] - Second release.
+
+* Improvements and test implementation.
+
+## [1.0.7] - Second release.
+
+* Improvements, test implementation and example file.
--- test/svg_to_paint_test.dart
+++ test/svg_to_paint_test.dart
@@ -1,7 +1,32 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:crypto/crypto.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('adds one to input values', () {
+  group('Test on file', () {
+    test('Check if file has been created ', () async {
+      await Process.run(
+          'flutter',
+          [
+            'pub',
+            'run',
+            'svg_to_paint',
+            '/Users/user/Desktop/google_icon.xml',
+            '/Users/user/Desktop',
+            'icon',
+            'absolute'
+          ],
+          runInShell: true);
 
+      expect(
+          File('/Users/user/Desktop/icon.dart').existsSync(), true);
+    });
+
+    test('Check with MD5 hash', () {
+      expect(
+          md5.convert(utf8.encode(File('/Users/user/Desktop/icon.dart').readAsStringSync())).toString(),
+          '3e13645991eb27fa8abb17e6abd240dc');
+    });
   });
 }
